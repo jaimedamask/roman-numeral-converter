14c98d71c7b4a5f08d9d50715c846140d2ed295c
jaimedamask <123277441+jaimedamask@users.noreply.github.com>
2024-05-28 18:09:57 -0400

    Fix: Correct parameters for convertNum function

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const number = document.getElementById('number');
 const convertBtn = document.getElementById('convert-btn');
 const output = document.getElementById('output');
 
-const convertNum = (number) => {
-    if (number <= 0) {
+const convertNum = () => {
+    const input = number.value;
+    if (input <= 0) {
         output.textContent = 'Please enter a number greater than or equal to 1';
-    } else if (number >= 4000) {
+    } else if (input >= 4000) {
         output.textContent = 'Please enter a number less than or equal to 3999';
     }
-}
+};
+
