8c4bc5a51109256be2523bb35e3686e0c8e2d03d
jaimedamask <123277441+jaimedamask@users.noreply.github.com>
2024-05-28 18:38:37 -0400

    Fix: Revise funtion to only check whether input is valid and set error message

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const number = document.getElementById('number');
 const convertBtn = document.getElementById('convert-btn');
 const output = document.getElementById('output');
+const input = number.value;
 
-const convertNum = () => {
-    const input = number.value;
-    if (input <= 0) {
-        output.textContent = 'Please enter a number greater than or equal to 1';
-    } else if (input >= 4000) {
-        output.textContent = 'Please enter a number less than or equal to 3999';
-    }
+
+const isValidInput = () => {
+    let errorMsg = '';
+    if (input < 1) {
+        errorMsg = 'Please enter a number greater than or equal to 1';
+    } else if (input > 3999) {
+        errorMsg = 'Please enter a number less than or equal to 3999';
+    } else return true;
 };
-
-convertBtn.addEventListener('click', convertNum);
