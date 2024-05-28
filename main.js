eb61c3ee4b3d96e360625d791357cef04e76a97c
jaimedamask <123277441+jaimedamask@users.noreply.github.com>
2024-05-28 18:43:13 -0400

    Feat: Add romanNumerals object to store Roman numerals and their Arabic numeral values

--- main.js
+++ main.js
@@ -2,7 +2,21 @@ const number = document.getElementById('number');
 const convertBtn = document.getElementById('convert-btn');
 const output = document.getElementById('output');
 const input = number.value;
-
+const romanNumerals = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+};
 
 const isValidInput = () => {
     let errorMsg = '';
