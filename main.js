ff556a50183e72fc558f63ae824b4ad7be31ccc5
jaimedamask <123277441+jaimedamask@users.noreply.github.com>
2024-05-28 18:03:50 -0400

    Feat: Add convertNum function and output for entries above and below valid range

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 const number = document.getElementById('number');
 const convertBtn = document.getElementById('convert-btn');
 const output = document.getElementById('output');
+
+const convertNum = (number) => {
+    if (number <= 0) {
+        output.textContent = 'Please enter a number greater than or equal to 1';
+    } else if (number >= 4000) {
+        output.textContent = 'Please enter a number less than or equal to 3999';
+    }
+}
